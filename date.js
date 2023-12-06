11439980397f6fbcb12c8955c9206e70bbfabb06
Abildaev Dastan <abildaev1996@gmail.com>
2023-12-07 01:23:46 +0600

    sdsd

--- date.js
+++ date.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/date', (req, res) => {
     res.send(new Date())
 })
 
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
-const fetch = require('node-fetch').default
 
 const app = express()
 
 app.get('/', (req, res) => {
-    fetch("http://date:3005").then(res => res.json()).then(data => {
-        res.send(`<h1>${data}</h1>`)
-    })
+    res.send(`<h1>hello world</h1>`)
 })
 
 app.listen(3000, () => {
