8bb08a5a277a289f9b7ac7d3034fad3ec3cd6084
Abildaev Dastan <abildaev1996@gmail.com>
2023-12-07 01:14:40 +0600

    adas

--- index.dockerfile
+++ index.dockerfile
@@ -1,6 +1,5 @@
 FROM node:20
 WORKDIR /app
-
 COPY . .
 RUN npm install
 EXPOSE 3000
--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const fetch = require('node-fetch').default
 
 const app = express()
 
-const DATE_SERVER_HOST = process.env.DATE_SERVER_HOST || "http://localhost:3005"
 app.get('/', (req, res) => {
-    fetch(DATE_SERVER_HOST).then(res => res.json()).then(data => {
+    fetch("http://date:3005").then(res => res.json()).then(data => {
         res.send(`<h1>${data}</h1>`)
     })
 })
