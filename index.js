d81723b82dbd5378214e9672d0cf9da4d77a7e89
Abildaev Dastan <abildaev1996@gmail.com>
2023-12-07 00:44:43 +0600

    ssssssss

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fetch = require('node-fetch').default
 
 const app = express()
 
-
+const DATE_SERVER_HOST = process.env.DATE_SERVER_HOST || "http://localhost:3005"
 app.get('/', (req, res) => {
-    fetch("https://date:3005").then(res => res.json()).then(data => {
+    fetch(DATE_SERVER_HOST).then(res => res.json()).then(data => {
         res.send(`<h1>${data}</h1>`)
     })
 })
